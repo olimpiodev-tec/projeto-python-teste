127c9ac0ff16f7303d102c709de3f74d6fa8b0bd
Moisés <olimpiodev.tec@gmail.com>
2026-03-06 10:49:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,5 +6,28 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!!!</h1>"
 
+@app.route("/curso")
+def curso():
+    return "<h1>Técnico em Desenvolvimento de Sistemas</h1>"
+
+@app.route("/cidade")
+def cidade():
+    return {
+        "nome": "Piracicaba",
+        "idade": 258,
+        "habitantes": 440000,
+        "fundacao": 1767
+    }
+
+@app.route("/musicas")
+def musicas():
+    return [
+        "Patati, patata",
+        "Meu lanchinho",
+        "Bananas de pijamas",
+        "Galinha pintadinha"
+    ]
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
